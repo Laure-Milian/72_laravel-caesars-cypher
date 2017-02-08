377a31330d42ef38cb1e09a34fb272a50fbada92
Laure <laure.milian@gmail.com>
2017-02-08 12:30:19 +0100

    page show ajax ok - route à créer

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,42 @@
-console.log('hey');
+(function() {
+
+	app = {
+
+		init: function() {
+			this.listeners();
+		},
+
+		listeners: function() {
+			$("#btnDecrypt").on('click', this.decrypt.bind(this));
+		},
+
+		decrypt: function() {
+			var offset = $("#offset").val();
+			var id = $("#id").val();
+			console.log(id);
+			$.ajaxSetup({
+				headers: {
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				}
+			});
+			$.ajax({
+				url: '/decrypt/' + id,
+				type: 'post'
+			})
+			.done(function(response) {
+				$("#decrypted_message").html(response);
+			})
+
+			// $.ajax({
+			// 	url: '/products/subtract/' + id,
+			// 	type: 'post'
+			// })
+			// .done(function(response) {
+			// 	$("#stock").html(response);
+			// });
+		}
+	}
+
+	app.init();
+
+})();
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,12 +9,17 @@
 
 <h2>Quelle clef utiliser ?</h2>
 <form action="">
+	<input id="id" type="hidden" value="{{$message->id}}">
 	<div class="field">
-	<label for="offset"></label>
-	<input id="offset" name="offset" type="number">
+		<label for="offset"></label>
+		<input id="offset" name="offset" type="number">
 	</div>
 	<div>
-		<input class="ui yellow button" type="submit">
+		<input id="btnDecrypt" class="ui yellow button" type="submit">
 	</div>
 </form>
+
+<h2>Le message à décrypter</h2>
+<div id="decrypted_message" class="ui segment"></div>
+
 @endsection
